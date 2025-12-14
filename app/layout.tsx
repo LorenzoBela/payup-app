8eb0436eac88f55ed6804021be2bcfd640f7687c
LorenzoBela <lorenzo.miguel.bela429@adamson.edu.ph>
2025-12-14 11:38:56 +0800

    Web Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { ClerkProvider } from '@clerk/nextjs'
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
@@ -47,6 +48,7 @@ export default function RootLayout({
             {children}
             <Toaster />
             <SpeedInsights />
+            <Analytics />
           </ThemeProvider>
         </body>
       </html>
